ccf335f87db1775e3409751dec006d3f838b0362
Kieran Andrews <hiddentiger@gmail.com>
2012-11-17 06:21:37 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swhealth::Application.config.secret_token = '648c15b9b0df66b492f96021a111ebf892c5f5ce6272341d25214560819339d8ce5c5b2a9f7cac0d7f06ebd7e496c802e85addc1daa476696a59bf8e8664ef90'
