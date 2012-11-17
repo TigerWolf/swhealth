0b4374dca81ab1873c623b1a89ffab0c567e2976
Kieran Andrews <hiddentiger@gmail.com>
2012-11-17 23:27:41 +0000

    Add relationship to models and modifications to the data scraping script

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,5 @@
 class Provider < ActiveRecord::Base
-#  belongs_to :categories
+  belongs_to :categories
 
-  attr_accessible :address1, :fax, :firstName, :glat, :glong, :name, :orgName, :orgSubType, :orgType, :phone, :postCode, :providerName, :providerNumber, :state, :suburb, :title
+  attr_accessible :address1, :fax, :firstName, :glat, :glong, :name, :orgName, :orgSubType, :orgType, :phone, :postCode, :providerName, :providerNumber, :state, :suburb, :title, :category_id
 end
--- lib/data/bupa_data.rb
+++ lib/data/bupa_data.rb
@@ -11,7 +11,7 @@ hash = array[0]
         p = Provider.create!(:name => provider['name'])
         p.address1 = provider['address1']
         ['fax','firstName','glat','glong','orgName','orgSubType','orgType','phone','postCode','providerName','providerNumber','state','suburb','title'].each do |prov|
-            p.prov = provider[prov]
+            p.update_attribute(prov.to_sym, provider[prov]) 
         end
         #p.update_attributes(provider.except(:address2, :address3, :branchInfo, :branchName, :brand, :contractType, :dataCode, :dementiaCare, :distance, :email, :externalEmail, :extraService, :gapCode, :gapCodeDescription, :gapTypeRank, :highCare, :hospitalGapType, :hospitalGapTypeDescription, :hospitalName, :hospitalType, :id, :initial1, :lowCare, :medicalSpecialty, :nmhiSpecCode1, :nmhiSpecCode2, :nmhiSpecCode3, :nmhiSpecCode4, :nmhiSpecCode5, :nmhiSpecCode6, :practiceId, :practitionerCode, :provFromDate, :provStatus, :provToDate, :specialtyCode1, :specialtyCode2, :specialtyCode3, :specialtyCode4, :specialtyCode5, :specialtyCode6, :specialtyType, :subType, :type, :utilRateCode, :utilRateDescription, :web))
         p.save!
