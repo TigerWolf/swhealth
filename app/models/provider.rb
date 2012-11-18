71b6f53bfe81144e91a5d8ad0da0845f0db202a3
Kieran Andrews <hiddentiger@gmail.com>
2012-11-18 00:17:09 +0000

    Adding new display logic

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,4 @@
 class Provider < ActiveRecord::Base
   belongs_to :categories
-
   attr_accessible :address1, :fax, :firstName, :glat, :glong, :name, :orgName, :orgSubType, :orgType, :phone, :postCode, :providerName, :providerNumber, :state, :suburb, :title, :category_id
 end
